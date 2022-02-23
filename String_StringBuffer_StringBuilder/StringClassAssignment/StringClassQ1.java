23b1c79664e50b9c3f231a370364c35ae5806bbb
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-02-23 18:00:07 +0530

    Create StringClassQ1.java

---
+++ String_StringBuffer_StringBuilder/StringClassAssignment/StringClassQ1.java
@@ -0,0 +1,9 @@
+public class StringClassQ1 {
+
+    public static void main(String[] args)
+    {
+        String str="Hello World";
+        int length = str.length();
+        System.out.println(length);
+    }
+}
