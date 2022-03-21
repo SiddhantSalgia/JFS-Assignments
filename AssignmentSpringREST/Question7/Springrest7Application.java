f6d988d8522aa8cdb729d16b58828b7d163f13b1
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 10:54:08 +0530

    Create Springrest7Application.java

---
+++ AssignmentSpringREST/Question7/Springrest7Application.java
@@ -0,0 +1,14 @@
+package Question7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrest7Application
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrest7Application.class, args);
+	}
+
+}
