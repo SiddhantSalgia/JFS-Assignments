9eb0c7c08875640a7240178748ed9916bac53fb8
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 11:05:30 +0530

    Create Test.java

---
+++ SpringCoreAssignment/Question1/Test.java
@@ -0,0 +1,22 @@
+package Question1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("q1.xml");
+
+        // using the setter injection
+        Customer customer=(Customer) context.getBean("customer");
+        customer.customerDetail();
+
+        // using constructor injection
+        Customer customerCon=(Customer) context.getBean("customerCon");
+        customerCon.customerDetail();
+
+    }
+
+}
