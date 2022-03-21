d633f440d8535b7fefb37ddff8357c1c0013053f
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 10:55:38 +0530

    Create Springrest8Application.java

---
+++ AssignmentSpringREST/Question8/Springrest8Application.java
@@ -0,0 +1,12 @@
+package Question8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrest8Application
+{
+    public static void main(String[] args) {
+        SpringApplication.run(Springrest8Application.class, args);
+    }
+}
