40e8c50012f5f21d3c78c71db55d902fc428485d
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 10:43:20 +0530

    Add files via upload

---
+++ AssignmentSpringREST/Question2/UserValidationController.java
@@ -0,0 +1,20 @@
+package Question2;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Scanner;
+
+@RestController
+public class UserValidationController
+{
+    @RequestMapping(value = "/validate/{username}/{password}",method = RequestMethod.GET)
+    public String validateUser(@PathVariable String  username, @PathVariable String password)
+    {
+        if(username.equals("Siddhant") && password.equals("Siddhant@2608"))
+            return "Valid User";
+        else
+            return "Invalid User";
+    }
+}
