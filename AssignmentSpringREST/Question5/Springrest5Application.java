4dfb3e9c1526064556e0b8d056c8e04563dc3874
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 10:48:04 +0530

    Create Springrest5Application.java

---
+++ AssignmentSpringREST/Question5/Springrest5Application.java
@@ -0,0 +1,13 @@
+package Question5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrest5Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Question5.Springrest5Application.class,args);
+    }
+}
