2b83cda2d44fc95c49306eb75b8647aadf1b9a08
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 10:51:59 +0530

    Add files via upload

---
+++ AssignmentSpringREST/Question6/Math.java
@@ -0,0 +1,36 @@
+package Question6;
+
+public class Math
+{
+    private int a,b;
+
+    public int getA()
+    {
+        return a;
+    }
+
+    public void setA(int a)
+    {
+        this.a = a;
+    }
+
+    public int getB()
+    {
+        return b;
+    }
+
+    public void setB(int b)
+    {
+        this.b = b;
+    }
+
+    public Math()
+    {
+    }
+
+    public Math(int a,int b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+}
