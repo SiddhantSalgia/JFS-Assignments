e81fca68337866c155f81d7f48daa0abcf98d4bc
SiddhantSalgia <78691721+SiddhantSalgia@users.noreply.github.com>
2022-03-21 10:58:04 +0530

    Create Springrest9Application.java

---
+++ AssignmentSpringREST/Question9/Springrest9Application.java
@@ -0,0 +1,12 @@
+package Question9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrest9Application
+{
+    public static void main(String[] args) {
+        SpringApplication.run(Question9.Springrest9Application.class, args);
+    }
+}
